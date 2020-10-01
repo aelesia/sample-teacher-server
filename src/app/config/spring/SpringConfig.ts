bcd7187599e7fcd83704500231d978f0eb19dbe3
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-10-01 20:04:33 +0800

    Setting up

--- src/app/config/spring/SpringConfig.ts
+++
@@ -1,7 +0,0 @@
-import { Cfg } from '../Cfg'
-
-if (process.env['NODE_ENV'] === 'test' || Cfg.ENVIRONMENT === 'test') {
-  require('./Spring.test')
-} else if (Cfg.ENVIRONMENT === 'local') {
-  require('./Spring.local')
-}
